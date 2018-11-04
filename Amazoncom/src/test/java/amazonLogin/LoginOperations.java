ea18321c5f3d7e55cea41263be285d81d2e2d1d2
sathyavani iragam <iragamreddy1986@gmail.com>
2018-11-04 08:31:53 -0600

    Updated LoginOperations.java and also added selenium-server-standalone-2.53.1.jar

--- Amazoncom/src/test/java/amazonLogin/LoginOperations.java
+++ Amazoncom/src/test/java/amazonLogin/LoginOperations.java
@@ -1,12 +1,15 @@
 package amazonLogin;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 
 public class LoginOperations{
 
 	@BeforeTest
 	public void openBrowser(){
-		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.amazon.com");
 	}
 	
 }
